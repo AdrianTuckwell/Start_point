b47c65f84cee1fbf3216240be953b2e93347a351
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-10-01 16:40:29 +0100

    both files seems to be working

--- pet_shop.rb
+++ pet_shop.rb
@@ -25,3 +25,82 @@ def increase_pets_sold(pet_shop,pets)
   pet_shop[:admin][:pets_sold] += pets
   return pets_sold(pet_shop)
 end
+
+# - get stock count
+def stock_count(pet_shop)
+  return pet_shop[:pets].count
+end
+
+# get count of breed of pet
+def pets_by_breed(pet_shop,breed)
+  breeds = []
+  pets_array = pet_shop[:pets]
+  for pet in pets_array
+    if pet[:breed] == breed
+      breeds.push(breed) 
+    end
+  end
+    # need to return an array of breeds
+    return breeds
+  end
+
+# get pet by name
+def find_pet_by_name(pet_shop,name)
+  pets_array = pet_shop[:pets]
+  for pet in pets_array
+    if pet[:name] == name
+      return pet
+    end
+  end
+  return nil
+end
+
+# remove pet by name
+def remove_pet_by_name(pet_shop,name)
+  pets_array = pet_shop[:pets]
+  pet_to_remove = find_pet_by_name(pet_shop,name)
+  pets_array.delete(pet_to_remove)
+end
+
+# add new pet
+def add_pet_to_stock(pet_shop,new_pet)
+  pets_array = pet_shop[:pets]
+  pets_array.push(new_pet)
+end
+
+# get customer pet count
+def customer_pet_count(customers)
+  return customers[:pets].count
+end
+
+# add pet to customer
+def add_pet_to_customer(customer,new_pet)
+ return customer[:pets].push(new_pet)
+end
+
+# check is customer has enough money for new pet
+def customer_can_afford_pet(customer,new_pet)
+  return customer[:cash] > new_pet[:price]
+end
+
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+# check is customer has enough money and pet exists?
+  if pet != nil && customer_can_afford_pet(customer,pet)
+    price = pet[:price]
+    # remove money from customer
+    customer[:cash] -= price
+    # add pet to customer 1
+    add_pet_to_customer(customer,pet)
+    # increase pets sold by 1
+    increase_pets_sold(pet_shop,1)
+    # add pet value to total cash of pet shop
+    add_or_remove_cash(pet_shop,price)
+  end
+end
+
+
+
+
+
+
